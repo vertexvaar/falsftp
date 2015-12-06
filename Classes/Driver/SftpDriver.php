92a9a5e4abc4f0ea31c35555d07254c771278796
Oliver Eglseder <oliver.eglseder@in2code.de>
2015-12-06 22:13:13 +0100

    [TASK] Add license stuff

--- Classes/Driver/SftpDriver.php
+++ Classes/Driver/SftpDriver.php
@@ -1,6 +1,22 @@
 <?php
 namespace VerteXVaaR\FalSftp\Driver;
 
+/* Copyright (C) 2015 Oliver Eglseder <php@vxvr.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 use TYPO3\CMS\Core\Charset\CharsetConverter;
 use TYPO3\CMS\Core\Resource\Driver\AbstractHierarchicalFilesystemDriver;
 use TYPO3\CMS\Core\Resource\Exception\FileOperationErrorException;
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,22 @@
 <?php
 
-/*
+/* Copyright (C) 2015 Oliver Eglseder <php@vxvr.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * For everyone: The driver registration ist not handled within the ext_tables.php
  * file because the driver would not be registered when the storage is not public.
  *
@@ -9,7 +25,6 @@
  * As you can see, it is an eID call. ext_tables.php is not loaded here.
  *
  */
-
 $bootFalSftp = function () {
     $driverRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
         \TYPO3\CMS\Core\Resource\Driver\DriverRegistry::class
