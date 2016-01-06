a4b263616f3016e1ecd0855845689d48d5dd263a
Oliver Eglseder <oliver.eglseder@in2code.de>
2016-01-07 00:34:12 +0100

    [TASK] Remove hacky driver registration. This should be done in the LocalConf

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,31 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
- * For everyone: The driver registration ist not handled within the ext_tables.php
- * file because the driver would not be registered when the storage is not public.
- *
- * Technical information:
- * The called URL looks like index.php?eID=dumpFile&t=p&p=2&token=xxx
- * As you can see, it is an eID call. ext_tables.php is not loaded here.
- *
- */
-$bootFalSftp = function () {
-    $driverRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Resource\Driver\DriverRegistry::class
-    );
-    $flexBuilder = new \VerteXVaaR\FalSftp\Environment\FlexBuilder();
-    if ($driverRegistry->registerDriverClass(
-        \VerteXVaaR\FalSftp\Driver\SftpDriver::class,
-        'Sftp',
-        'SFTP Driver',
-        $flexBuilder->getFlexConfiguration()
-    )
-    ) {
-        $driverRegistry->addDriversToTCA();
-    }
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['FileInfo']['fileExtensionToMimeType']['png'] = 'image/png';
-};
-
-$bootFalSftp();
-unset($bootFalSftp);
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['FileInfo']['fileExtensionToMimeType']['png'] = 'image/png';
